483debaecaa6597bfd1ea82e46e73fcb1f3eae38
BlackFox101 <sad_alexxxei@@mail.ru>
2020-04-19 17:18:11 +0300

    Избавился от магического числа,  переделал функцию EXCLUDE в вычитание множеств

--- lw19/19.1/191.pas
+++ lw19/19.1/191.pas
@@ -2,13 +2,14 @@ PROGRAM Prime(INPUT, OUTPUT);
 {Находит простые числа в  диапазоне, ограниченном от 2 до 100,
   методом «Решето Эратосфена» }
 CONST
+  Min = 2;
   Max = 100;
 VAR
-  Sieve: SET OF 0..Max; // Исходно множество из которого будем выбирать простые чиса 
+  Sieve: SET OF 0..Max; // Исходно множество из которого будем выбирать простые числа 
   PrimeDigit, TempDigit: INTEGER;
 BEGIN{Prime}
-  Sieve := [2..Max];
-  PrimeDigit := 2; // Первое простое число
+  Sieve := [Min .. Max];
+  PrimeDigit := Min; // Первое простое число
   WHILE Sieve <> [] 
   DO
     BEGIN
@@ -16,14 +17,13 @@ BEGIN{Prime}
       WHILE TempDigit  <= Max // Убираем составные числа
       DO
         BEGIN
-          EXCLUDE(Sieve, TempDigit);
+          Sieve := Sieve - [TempDigit];
           TempDigit := TempDigit + PrimeDigit;
-        END;
-        
+        END;       
       WRITE(PrimeDigit, ' '); // Выводим простое число
-      
       WHILE NOT(PrimeDigit IN Sieve) AND (PrimeDigit < Max)
       DO
         PrimeDigit := PrimeDigit + 1;
     END;
+  WRITELN;
 END.{Prime}
