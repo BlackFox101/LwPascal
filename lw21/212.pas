859f778e8cf9cdbfc63632208af34b132c21c1da
BlackFox101 <sad_alexxxei@@mail.ru>
2020-04-30 23:21:27 +0300

    Add lw21

---
+++ lw21/212.pas
@@ -0,0 +1,98 @@
+PROGRAM Encryption(INPUT, OUTPUT);
+{Переводит символы из INPUT в код согласно Chiper
+  и печатает новые символы в OUTPUT}
+CONST
+  MaxLen = 20;
+TYPE
+  Str = ARRAY [1 .. MaxLen] OF 'A' .. 'Z';
+  Chiper = ARRAY ['A' .. 'Z'] OF CHAR;
+VAR
+  Msg: Str;
+  Code: Chiper;
+  LenStr: 1 .. MaxLen;
+  ChiperFile, EncryptedMsg: TEXT; // Формата "Буква-ШифрБуквы"
+
+PROCEDURE Initialize(VAR FInChiper: TEXT);
+{Присвоить Code шифр замены}
+var
+  Letter, ChiperLetter: CHAR;
+  I: CHAR;
+BEGIN {Initialize}
+  FOR I := 'A' TO 'Z'
+  DO
+    Code[I] := I; // Сначал все буквы зашифруем как "сами себе"
+  RESET(FInChiper);
+  WHILE NOT EOF(FInChiper)
+  DO
+    BEGIN
+      WHILE NOT EOLN(FInChiper)
+      DO
+        BEGIN
+          READ(FInChiper, Letter); //Буква которую хотим зашифровать
+          IF NOT EOLN(FInChiper)
+          THEN
+            READ(FInChiper, ChiperLetter); //Пропустим '-'
+          IF NOT EOLN(FInChiper)
+          THEN
+            READ(FInChiper, ChiperLetter); //Прочитаем шифр буквы
+          IF Letter IN ['A' .. 'Z']
+          THEN
+            Code[Letter] := ChiperLetter;
+        END;
+      READLN(FInChiper);
+    END;
+END;  {Initialize}
+
+PROCEDURE Encode(VAR MsgStr: Str; LenStr: INTEGER);
+{Выводит символы из Code, соответствующие символам из S}
+VAR
+  Index: 1 .. MaxLen;
+BEGIN {Encode}
+  FOR Index := 1 TO LenStr
+  DO
+    IF MsgStr[Index] IN ['A' .. 'Z']
+    THEN
+      BEGIN
+        WRITE(OUTPUT, Code[MsgStr[Index]]);
+        WRITE(EncryptedMsg, Code[MsgStr[Index]]) //Запись в файл который будем расшифровывать в 21.3
+      END
+    ELSE
+      IF MsgStr[Index] = ' '
+      THEN
+        BEGIN
+          WRITE(OUTPUT, '_');
+          WRITE(EncryptedMsg, '_')
+        END
+      ELSE
+        BEGIN
+          WRITE(OUTPUT, MsgStr[Index]);
+          WRITE(EncryptedMsg, MsgStr[Index])
+        END;
+  WRITELN(EncryptedMsg);
+  WRITELN
+END;  {Encode}
+
+BEGIN {Encryption}
+  ASSIGN(ChiperFile, 'ChiperFile.TXT');
+  ASSIGN(EncryptedMsg, 'EncryptedMsg.TXT');
+  {Инициализировать Code}
+  Initialize(ChiperFile);
+  REWRITE(EncryptedMsg);
+  WHILE NOT EOF(INPUT)
+  DO
+    BEGIN
+      {читать строку в Msg и распечатать ее}
+      LenStr := 1;
+      WHILE NOT EOLN(INPUT) AND (LenStr < MaxLen + 1)
+      DO
+        BEGIN
+          READ(INPUT, Msg[LenStr]);
+          WRITE(OUTPUT, Msg[LenStr]);
+          LenStr := LenStr + 1
+        END;
+      READLN;
+      WRITELN;
+      {распечатать кодированное сообщение}
+      Encode(Msg, LenStr)
+    END
+END.  {Encryption}
---
+++ lw21/213.pas
@@ -0,0 +1,89 @@
+PROGRAM Encryption(INPUT, OUTPUT);
+{Переводит символы из INPUT в код согласно Chiper
+  и печатает новые символы в OUTPUT}
+CONST
+  MaxLen = 20;
+  EngLetters = 26;
+TYPE
+  Str = ARRAY [1 .. MaxLen] OF CHAR;
+  Chiper = ARRAY ['!' .. 'Z'] OF CHAR;
+VAR
+  Msg: Str;
+  Code: Chiper;
+  LenStr: 1 .. MaxLen;
+  ChiperFile, EncryptedMsg: TEXT; // Формата "Буква-ШифрБуквы"
+
+PROCEDURE Initialize(VAR FInChiper: TEXT);
+{Присвоить Code шифр замены}
+var
+  Letter, ChiperLetter: CHAR;
+  I: CHAR;
+BEGIN {Initialize}
+  FOR I := '!' TO 'Z'
+  DO
+    Code[I] := I; // Сначал все буквы зашифруем как "сами себе"
+  RESET(FInChiper);
+  WHILE NOT EOF(FInChiper)
+  DO
+    BEGIN
+      WHILE NOT EOLN(FInChiper)
+      DO
+        BEGIN
+          READ(FInChiper, Letter); //Буква которую хотим зашифровать
+          IF NOT EOLN(FInChiper)
+          THEN
+            READ(FInChiper, ChiperLetter); //Пропустим '-'
+          IF NOT EOLN(FInChiper)
+          THEN
+            READ(FInChiper, ChiperLetter); //Прочитаем шифр буквы
+          IF Letter IN ['A' .. 'Z']
+          THEN
+            Code[ChiperLetter] := Letter;
+          //WRITELN(ChiperLetter, ' ', Code[ChiperLetter]);
+        END;
+      READLN(FInChiper);
+    END;
+    //WRITELN('Code[', I,  ']=''', Code[I], '''');
+END;  {Initialize}
+
+PROCEDURE Encode(VAR MsgStr: Str; LenStr: INTEGER);
+{Выводит символы из Code, соответствующие символам из S}
+VAR
+  Index: 1 .. MaxLen;
+BEGIN {Encode}
+  FOR Index := 1 TO LenStr
+  DO
+    IF MsgStr[Index] IN ['!' .. 'Z']
+    THEN
+      WRITE(OUTPUT, Code[MsgStr[Index]])
+    ELSE
+      IF MsgStr[Index] = ' '
+      THEN
+        WRITE(OUTPUT, '_')
+      ELSE
+        WRITE(OUTPUT, MsgStr[Index]);
+  WRITELN
+END;  {Encode}
+
+BEGIN {Encryption}
+  ASSIGN(ChiperFile, 'ChiperFile.TXT');
+  {Инициализировать Code}
+  Initialize(ChiperFile);
+  WHILE NOT EOF(EncryptedMsg)
+  DO
+    BEGIN
+      {читать строку в Msg и распечатать ее}
+      LenStr := 1;
+      WHILE NOT EOLN(EncryptedMsg) AND (LenStr < MaxLen + 1)
+      DO
+      BEGIN
+        READ(EncryptedMsg, Msg[LenStr]);
+        WRITE(OUTPUT, Msg[LenStr]);
+        LenStr := LenStr + 1;
+      END;
+      READLN;
+      WRITELN;
+      {распечатать кодированное сообщение}
+      Encode(Msg, LenStr)
+    END;
+END.  {Encryption}
